97f0963579c0fc372b13009058a430d6d75e68fa
Carolina <mamanicarolina27@gmail.com>
2024-04-18 13:01:34 -0300

    Fixing somethin error on css or html

---
+++ main.js
@@ -0,0 +1,51 @@
+//References to DOM elements
+const prevBtn = document.querySelector('#prev=btn');
+const nextBtn = document.querySelector('#next-btn');
+const book = document.querySelector('#book');
+
+const paper1 = document.querySelector('#p1');
+const paper2 = document.querySelector('#p2');
+const paper3 = document.querySelector('#p3');
+
+//Event Listener
+prevBtn.addEventListener("click", goPrevPage);
+nextBtn.addEventListener('click', goNetxPage)
+
+//Business Logic
+let currentLocation = 1;
+let numOfPapers = 3;
+let maxLocation = numOfPapers + 1;
+
+
+function openBook(){
+
+}
+
+function closeBook(){
+
+}
+
+function goNetxPage(){
+ if(currentLocation < maxLocation){
+    switch(currentLocation){
+        case 1:
+            openBook();
+            paper1.classList.add('flipped');
+            break;
+        case 2:
+            paper2.classList.add('flipped');
+            break;
+        case 3:
+            paper3.classList.add('flipped');
+            closeBook();
+            break;
+        default:
+            throw new Error('Unkow state')
+    }
+    currentLocation++;
+ }
+}
+
+function goPrevPage(){
+
+}
